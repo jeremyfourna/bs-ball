cd2979404b03ec87c651067baf04faa8df95a866
jeremyfourna <jeremyfourna@gmail.com>
2017-09-14 22:31:02 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const R = require('ramda');
+const { createCircle } = require('bs-geometric-forms');
+
+function addBallToGame(player, allPlayers) {
+  const ballPosition = R.prop(player, R.pick([player], allPlayers));
+  return R.assoc('className', 'ball', ballPosition);
+}
+
+function generateBallPosition(wishedZoom, ballPosition, context) {
+  createCircle(
+    R.prop('x', ballPosition),
+    R.prop('y', ballPosition),
+    // Impure because access wishedZoom outside the function
+    R.multiply(wishedZoom, 0.25),
+    'black',
+    'orange',
+    R.prop('className', ballPosition),
+    context
+  );
+}
+
+exports.addBallToGame = addBallToGame;
+exports.generateBallPosition = generateBallPosition;
